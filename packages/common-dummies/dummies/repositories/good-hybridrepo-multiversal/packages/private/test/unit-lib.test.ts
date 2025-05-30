420c8b62b0a0191fac7b4cbbb76caa2e57a70884
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-05-29 19:23:05 -0700

    fix(packages/common-dummies): use correct import syntax for dummy testverse imports

--- packages/common-dummies/dummies/repositories/good-hybridrepo-multiversal/packages/private/test/unit-lib.test.ts
+++ packages/common-dummies/dummies/repositories/good-hybridrepo-multiversal/packages/private/test/unit-lib.test.ts
@@ -1,6 +1,6 @@
 import { thing } from 'universe+private:lib/library.ts';
 import { version } from 'rootverse+private:package.json';
-import { something } from 'testverse setup.ts';
+import { something } from 'testverse:setup.ts';
 
 test('fake test -- very bad if you see this in your CLI', () => {
   void thing;
